6a1a050a8605d48af029361b5db347d4ca2020ea
vmoccia-at-253840653716 <amlq8eeJlRIps63sE5SBcFx6BkOsdjlBkGBXHhIgcro=>
2017-07-24 11:40:48 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,14 @@
+**HOW TO USE AWS COGNITO FOR AUTHENTICATION AND AUTHORIZATION**
+
+In this project I've configured AWS COGNITO in "Federation Identity mode" using COGNITO as Idp.
+
+Before to start the project remember to set the correct parameters in config.js and create 
+an user on cognito's user pool.
+
+For starting the project:
+
+`$ npm install`
+
+`$ npm start`
+
+
--- src/config.js
+++ src/config.js
@@ -7,14 +7,14 @@ const authenticationData =  {
 };
 
 const poolData = {
-    UserPoolId : envs('USER_POOL_ID', 'eu-west-1_ZyVqqWzfo'), // Your user pool id here
-    ClientId : envs('CLIENT_ID','47ksvk8cl9d6455ekea5gevm66'), // Your client id here
+    UserPoolId : envs('USER_POOL_ID', 'eu-west-1_*********'), // Your user pool id here
+    ClientId : envs('CLIENT_ID','47ksvk8cl9d6455ek*********'), // Your client id here
 };
 
 const region = 'eu-west-1';
 
 const cognitoIdpUrl = 'cognito-idp.'+ region +'.amazonaws.com/'; //EU-WEST-1;
-const identityPoolId = envs('IDENTITY_POOL_ID', 'eu-west-1:36f219b6-8bd8-4275-a96d-9474b22d0028');
+const identityPoolId = envs('IDENTITY_POOL_ID', 'eu-west-1:36f219b6-8bd8-4275-a96d-************');
 
 const aws =  {
     region: region,
