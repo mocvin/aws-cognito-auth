90049bdec516c063d20c9c1e8c0432977a543fb3
vmoccia-at-253840653716 <amlq8eeJlRIps63sE5SBcFx6BkOsdjlBkGBXHhIgcro=>
2017-07-24 12:03:27 +0200

    Fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const  envs = require('envs');
+const region = 'eu-west-1';
+
 const authenticationData =  {
     Username: envs('USERNAME', 'vinmoc'),
     Password: envs('PASSWORD', 'Password123')
@@ -10,8 +12,6 @@ const poolData = {
     UserPoolId : envs('USER_POOL_ID', 'eu-west-1_*********'), // Your user pool id here
     ClientId : envs('CLIENT_ID','47ksvk8cl9d6455ek*********'), // Your client id here
 };
-
-const region = 'eu-west-1';
 
 const cognitoIdpUrl = 'cognito-idp.'+ region +'.amazonaws.com/'; //EU-WEST-1;
 const identityPoolId = envs('IDENTITY_POOL_ID', 'eu-west-1:36f219b6-8bd8-4275-a96d-************');
